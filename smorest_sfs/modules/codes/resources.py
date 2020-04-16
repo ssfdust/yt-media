defbaa7cc586d4ca5bab1e4f74771f5bff6c2279
ssfdust <ssfdust@gmail.com>
2020-04-16 18:31:00 +0800

    2020.4.3

--- smorest_sfs/modules/codes/resources.py
+++ smorest_sfs/modules/codes/resources.py
@@ -9,8 +9,7 @@ from typing import Any, Dict
 from flask.views import MethodView
 
 from smorest_sfs.modules.auth import PERMISSIONS
-from smorest_sfs.modules.auth.decorators import (doc_login_required,
-                                                 permission_required)
+from smorest_sfs.modules.auth.decorators import doc_login_required, permission_required
 
 from . import blp, models, schemas
 
--- tests/utils/test_text.py
+++ tests/utils/test_text.py
@@ -6,8 +6,12 @@ from smorest_sfs.utils.text import camel_to_snake
 
 
 @pytest.mark.parametrize(
-    "camel, snake", [("AdminAvator", "admin_avator"), ("getHTTPResponseCode", "get_http_response_code"),
-                     ("HTTPResponseCodeXYZ", "http_response_code_xyz")]
+    "camel, snake",
+    [
+        ("AdminAvator", "admin_avator"),
+        ("getHTTPResponseCode", "get_http_response_code"),
+        ("HTTPResponseCodeXYZ", "http_response_code_xyz"),
+    ],
 )
 def test_check_ext_success(camel: str, snake: str) -> None:
     assert camel_to_snake(camel) == snake
