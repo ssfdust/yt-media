9e50ffe4bce5ae3e3caec64313c998ee937ca241
ssfdust <ssfdust@gmail.com>
2020-04-23 19:42:16 +0800

    add readme

--- tasks/app/db.py
+++ tasks/app/db.py
@@ -9,134 +9,285 @@ import argparse
 import logging
 import os
 
-from flask import current_app
-
 from ._utils import app_context_task
 
-log = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+log = logging.getLogger(__name__) # pylint: disable=invalid-name
 
 try:
     from alembic import __version__ as __alembic_version__
     from alembic.config import Config as AlembicConfig
     from alembic import command
 except ImportError:
-    log.warning("Alembic can't be imported, some app.db.* tasks won't be available!")
+    log.warning("Alembic cannot be imported, so some app.db.* tasks won't be available!")
 else:
 
-    alembic_version = tuple([int(v) for v in __alembic_version__.split(".")[0:3]])
+    alembic_version = tuple([int(v) for v in __alembic_version__.split('.')[0:3]])
+
 
     class Config(AlembicConfig):
         """
-        自定义配置
+        Custom config that overwrites template directory.
         """
-
         def get_template_directory(self):
             package_dir = os.path.abspath(os.path.dirname(__file__))
-            return os.path.join(package_dir, "db_templates")
+            return os.path.join(package_dir, 'db_templates')
 
 
 def _get_config(directory, x_arg=None, opts=None):
     """
-    预准备AlembicConfig实例
+    A helper that prepares AlembicConfig instance.
     """
-    config = Config(os.path.join(directory, "alembic.ini"))
-    config.set_main_option("script_location", directory)
+    config = Config(os.path.join(directory, 'alembic.ini'))
+    config.set_main_option('script_location', directory)
     if config.cmd_opts is None:
         config.cmd_opts = argparse.Namespace()
     for opt in opts or []:
         setattr(config.cmd_opts, opt, True)
     if x_arg is not None:
-        if not getattr(config.cmd_opts, "x", None):
-            setattr(config.cmd_opts, "x", [x_arg])
+        if not getattr(config.cmd_opts, 'x', None):
+            setattr(config.cmd_opts, 'x', [x_arg])
         else:
             config.cmd_opts.x.append(x_arg)
     return config
 
-
 @app_context_task(
-    help={"directory": "迁移脚本目录", "multidb": "迁移复数个数据库",}
+    help={
+        'directory': "migration script directory",
+        'multidb': "Multiple databases migraton",
+    }
 )
-def init(context, directory="migrations", multidb=False):
-    """初始化迁移脚本"""
-    if directory is None:
-        directory = current_app.extensions["migrate"].directory
+def init(context, directory='migrations', multidb=False):
+    """Generates a new migration"""
     config = Config()
-    config.set_main_option("script_location", directory)
-    config.config_file_name = os.path.join(directory, "alembic.ini")
-    print(config.get_template_directory())
-    config = current_app.extensions["migrate"].migrate.call_configure_callbacks(config)
+    config.set_main_option('script_location', directory)
+    config.config_file_name = os.path.join(directory, 'alembic.ini')
     if multidb:
-        command.init(config, directory, "flask-multidb")
+        command.init(config, directory, 'flask-multidb')
     else:
-        command.init(config, directory, "flask")
+        command.init(config, directory, 'flask')
 
 
 @app_context_task(
     help={
-        "rev_id": "手动指定revision id",
-        "version_path": "Specify specific path from config for version file",
-        "branch_label": "Specify a branch label to apply to the new revision",
-        "splice": "Allow a non-head revision as the 'head' to splice onto",
-        "head": "选择一个revision <branchname>@head to base new revision on",
-        "sql": "显示待执行的Sql语句",
-        "directory": "迁移脚本目录",
+        'rev_id': "Specify a hardcoded revision id instead of generating one",
+        'version_path': "Specify specific path from config for version file",
+        'branch_label': "Specify a branch label to apply to the new revision",
+        'splice': "Allow a non-head revision as the 'head' to splice onto",
+        'head': "Specify head revision or <branchname>@head to base new revision on",
+        'sql': "Don't emit SQL to database - dump to standard output instead",
+        'autogenerate': "Populate revision script with andidate migration operatons, based " \
+                        "on comparison of database to model",
+        'directory': "migration script directory",
     }
 )
-def migrate(
-    context, directory="migrations", message=None, sql=False,
-):
-    """'revision --autogenerate'的简写"""
-    config = _get_config(directory, opts=["autogenerate"])
-    command.revision(config, message, autogenerate=True, sql=sql)
-
-
-@app_context_task(help={"revision": "revision标志", "directory": "迁移脚本目录"})
-def edit(context, revision="current", directory="migrations"):
-    """编辑一个迁移脚本"""
+def revision(context, directory='migrations', message=None, autogenerate=False, sql=False,
+             head='head', splice=False, branch_label=None, version_path=None, rev_id=None):
+    """Create a new revision file."""
     config = _get_config(directory)
-    command.edit(config, revision)
-
-
-@app_context_task(
-    help={"revision": "版本号", "directory": "迁移脚本目录",}
-)
-def show(context, directory="migrations", revision="head"):
-    """显示数据库更改详情信息"""
-    config = _get_config(directory)
-    command.show(config, revision)
-
+    if alembic_version >= (0, 7, 0):
+        command.revision(config, message, autogenerate=autogenerate, sql=sql,
+                         head=head, splice=splice, branch_label=branch_label,
+                         version_path=version_path, rev_id=rev_id)
+    else:
+        command.revision(config, message, autogenerate=autogenerate, sql=sql)
 
 @app_context_task(
     help={
-        "tag": "Arbitrary 'tag' name - can be used by custom env.py scripts",
-        "sql": "显示待执行的Sql语句",
-        "revision": "版本号",
-        "directory": "迁移脚本目录",
-        "x_arg": "经由自定义env.py脚本处理的额外参数",
+        'rev_id': "Specify a hardcoded revision id instead of generating one",
+        'version_path': "Specify specific path from config for version file",
+        'branch_label': "Specify a branch label to apply to the new revision",
+        'splice': "Allow a non-head revision as the 'head' to splice onto",
+        'head': "Specify head revision or <branchname>@head to base new revision on",
+        'sql': "Don't emit SQL to database - dump to standard output instead",
+        'directory': "migration script directory",
     }
 )
-def upgrade(
-    context, directory="migrations", revision="head", sql=False, tag=None, x_arg=None,
-):
-    """更新下一个数据库版本"""
+def migrate(context, directory='migrations', message=None, sql=False, head='head', splice=False,
+            branch_label=None, version_path=None, rev_id=None):
+    """Alias for 'revision --autogenerate'"""
+    config = _get_config(directory, opts=['autogenerate'])
+    if alembic_version >= (0, 7, 0):
+        command.revision(config, message, autogenerate=True, sql=sql, head=head,
+                         splice=splice, branch_label=branch_label,
+                         version_path=version_path, rev_id=rev_id)
+    else:
+        command.revision(config, message, autogenerate=True, sql=sql)
+
+@app_context_task(
+    help={
+        'revision': "revision identifier",
+        'directory': "migration script directory",
+    }
+)
+def edit(context, revision='current', directory='migrations'):
+    """Upgrade to a later version"""
+    if alembic_version >= (0, 8, 0):
+        config = _get_config(directory)
+        command.edit(config, revision)
+    else:
+        raise RuntimeError('Alembic 0.8.0 or greater is required')
+
+@app_context_task(
+    help={
+        'rev_id': "Specify a hardcoded revision id instead of generating one",
+        'branch_label': "Specify a branch label to apply to the new revision",
+        'message': "one or more revisions, or 'heads' for all heads",
+        'directory': "migration script directory",
+    }
+)
+def merge(context, directory='migrations', revisions='', message=None, branch_label=None,
+          rev_id=None):
+    """Merge two revisions together.  Creates a new migration file"""
+    if alembic_version >= (0, 7, 0):
+        config = _get_config(directory)
+        command.merge(config, revisions, message=message,
+                      branch_label=branch_label, rev_id=rev_id)
+    else:
+        raise RuntimeError('Alembic 0.7.0 or greater is required')
+
+@app_context_task(
+    help={
+        'tag': "Arbitrary 'tag' name - can be used by custom env.py scripts",
+        'sql': "Don't emit SQL to database - dump to standard output instead",
+        'revision': "revision identifier",
+        'directory': "migration script directory",
+        'x_arg': "Additional arguments consumed by custom env.py scripts",
+    }
+)
+def upgrade(context, directory='migrations', revision='head', sql=False, tag=None, x_arg=None,
+            app=None):
+    """Upgrade to a later version"""
     config = _get_config(directory, x_arg=x_arg)
     command.upgrade(config, revision, sql=sql, tag=tag)
 
 
 @app_context_task(
     help={
-        "tag": "Arbitrary 'tag' name - can be used by custom env.py scripts",
-        "sql": "显示待执行的Sql语句",
-        "revision": "revision标志",
-        "directory": "迁移脚本目录",
-        "x_arg": "经由自定义env.py脚本处理的额外参数",
+        'tag': "Arbitrary 'tag' name - can be used by custom env.py scripts",
+        'sql': "Don't emit SQL to database - dump to standard output instead",
+        'revision': "revision identifier",
+        'directory': "migration script directory",
+        'x_arg': "Additional arguments consumed by custom env.py scripts",
     }
 )
-def downgrade(
-    context, directory="migrations", revision="-1", sql=False, tag=None, x_arg=None,
-):
-    """回退到上一个数据哭版本"""
+def downgrade(context, directory='migrations', revision='-1', sql=False, tag=None, x_arg=None):
+    """Revert to a previous version"""
     config = _get_config(directory, x_arg=x_arg)
-    if sql and revision == "-1":
-        revision = "head:-1"
+    if sql and revision == '-1':
+        revision = 'head:-1'
     command.downgrade(config, revision, sql=sql, tag=tag)
+
+@app_context_task(
+    help={
+        'revision': "revision identifier",
+        'directory': "migration script directory",
+    }
+)
+def show(context, directory='migrations', revision='head'):
+    """Show the revision denoted by the given symbol."""
+    if alembic_version >= (0, 7, 0):
+        config = _get_config(directory)
+        command.show(config, revision)
+    else:
+        raise RuntimeError('Alembic 0.7.0 or greater is required')
+
+@app_context_task(
+    help={
+        'verbose': "Use more verbose output",
+        'rev_range': "Specify a revision range; format is [start]:[end]",
+        'directory': "migration script directory",
+    }
+)
+def history(context, directory='migrations', rev_range=None, verbose=False):
+    """List changeset scripts in chronological order."""
+    config = _get_config(directory)
+    if alembic_version >= (0, 7, 0):
+        command.history(config, rev_range, verbose=verbose)
+    else:
+        command.history(config, rev_range)
+
+@app_context_task(
+    help={
+        'resolve_dependencies': "Treat dependency versions as down revisions",
+        'verbose': "Use more verbose output",
+        'directory': "migration script directory",
+    }
+)
+def heads(context, directory='migrations', verbose=False, resolve_dependencies=False):
+    """Show current available heads in the script directory"""
+    if alembic_version >= (0, 7, 0):
+        config = _get_config(directory)
+        command.heads(config, verbose=verbose,
+                      resolve_dependencies=resolve_dependencies)
+    else:
+        raise RuntimeError('Alembic 0.7.0 or greater is required')
+
+@app_context_task(
+    help={
+        'verbose': "Use more verbose output",
+        'directory': "migration script directory",
+    }
+)
+def branches(context, directory='migrations', verbose=False):
+    """Show current branch points"""
+    config = _get_config(directory)
+    if alembic_version >= (0, 7, 0):
+        command.branches(config, verbose=verbose)
+    else:
+        command.branches(config)
+
+@app_context_task(
+    help={
+        'head_only': "Deprecated. Use --verbose for additional output",
+        'verbose': "Use more verbose output",
+        'directory': "migration script directory",
+    }
+)
+def current(context, directory='migrations', verbose=False, head_only=False):
+    """Display the current revision for each database."""
+    config = _get_config(directory)
+    if alembic_version >= (0, 7, 0):
+        command.current(config, verbose=verbose, head_only=head_only)
+    else:
+        command.current(config)
+
+@app_context_task(
+    help={
+        'tag': "Arbitrary 'tag' name - can be used by custom env.py scripts",
+        'sql': "Don't emit SQL to database - dump to standard output instead",
+        'revision': "revision identifier",
+        'directory': "migration script directory",
+    }
+)
+def stamp(context, directory='migrations', revision='head', sql=False, tag=None):
+    """'stamp' the revision table with the given revision; don't run any
+    migrations"""
+    config = _get_config(directory)
+    command.stamp(config, revision, sql=sql, tag=tag)
+
+
+@app_context_task
+def init_development_data(context, upgrade_db=True, skip_on_failure=False):
+    """
+    Fill a database with development data like default users.
+    """
+    if upgrade_db:
+        context.invoke_execute(context, 'app.db.upgrade')
+
+    log.info("Initializing development data...")
+
+    from migrations import initial_development_data
+    try:
+        initial_development_data.init()
+    except AssertionError as exception:
+        if not skip_on_failure:
+            log.error("%s", exception)
+        else:
+            log.debug(
+                "The following error was ignored due to the `skip_on_failure` flag: %s",
+                exception
+            )
+            log.info("Initializing development data step is skipped.")
+    else:
+        log.info("Fixtures have been successfully applied.")
